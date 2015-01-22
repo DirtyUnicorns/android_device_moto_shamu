4a094a3d4dc6a566ce13a7e48a3dd8d4461ed782
zephiK <zephik@gmail.com>
2015-01-22 16:30:38 -0500

    shamu: disable mp-decision logspam Logcat is spammed with POWER_HINT_INTERACTION and failed to send touch boost when mpdecision is not being used.
    Change-Id: Ie6dc2f94845967e6873b12dc52025dfd36322ad9

--- power/power_shamu.c
+++ power/power_shamu.c
@@ -30,6 +30,7 @@
 #include <stdbool.h>
 
 #define LOG_TAG "PowerHAL"
+#define LOG_NDEBUG 0
 #include <utils/Log.h>
 
 #include <hardware/hardware.h>
@@ -87,7 +88,7 @@ static void coresonline(int off)
     }
 
     if (rc < 0) {
-        ALOGE("%s: failed to send: %s", __func__, strerror(errno));
+        //ALOGE("%s: failed to send: %s", __func__, strerror(errno));
     }
 }
 
@@ -115,7 +116,7 @@ static void enc_boost(int off)
     }
 
     if (rc < 0) {
-        ALOGE("%s: failed to send: %s", __func__, strerror(errno));
+        //ALOGE("%s: failed to send: %s", __func__, strerror(errno));
     }
 }
 
@@ -166,7 +167,7 @@ static void touch_boost()
     rc = sendto(client_sockfd, data, strlen(data), 0,
         (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
     if (rc < 0) {
-        ALOGE("%s: failed to send: %s", __func__, strerror(errno));
+        //ALOGE("%s: failed to send: %s", __func__, strerror(errno));
     }
 }
 
@@ -187,13 +188,13 @@ static void low_power(int on)
         snprintf(data, MAX_LENGTH, "10:%d", client);
         rc = sendto(client_sockfd, data, strlen(data), 0, (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
         if (rc < 0) {
-            ALOGE("%s: failed to send: %s", __func__, strerror(errno));
+            //ALOGE("%s: failed to send: %s", __func__, strerror(errno));
         }
     } else {
         snprintf(data, MAX_LENGTH, "9:%d", client);
         rc = sendto(client_sockfd, data, strlen(data), 0, (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
         if (rc < 0) {
-            ALOGE("%s: failed to send: %s", __func__, strerror(errno));
+            //ALOGE("%s: failed to send: %s", __func__, strerror(errno));
         }
     }
 }
@@ -235,7 +236,7 @@ static void power_hint( __attribute__((unused)) struct power_module *module,
 {
     switch (hint) {
         case POWER_HINT_INTERACTION:
-            ALOGV("POWER_HINT_INTERACTION");
+            //ALOGV("POWER_HINT_INTERACTION");
             touch_boost();
             break;
 #if 0
