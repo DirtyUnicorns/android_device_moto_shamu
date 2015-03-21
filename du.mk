b92df90f1ee7c89aa3126a55b967d83263dcaa14
Josh Chasky <jchasky@gmail.com>
2015-03-21 15:02:31 -0400

    Shamu: Update to Google's style of vendor
    Change-Id: I7111abdab7d62f1d772769518645917e4bc57132

--- du.mk
+++ du.mk
@@ -32,7 +32,7 @@ PRODUCT_MANUFACTURER := motorola
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=shamu BUILD_FINGERPRINT=google/shamu/shamu:5.0.1/LRX22C/1602158:user/release-keys PRIVATE_BUILD_DESC="shamu-user 5.0.1 LXR22C 1602158 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=shamu BUILD_FINGERPRINT=google/shamu/shamu:5.1.0/LMY47E/1602158:user/release-keys PRIVATE_BUILD_DESC="shamu-user 5.1.0 LMY47E 1602158 release-keys"
 
 $(call inherit-product, device/moto/shamu/device.mk)
-$(call inherit-product-if-exists, vendor/motorola/shamu/shamu-vendor.mk)
+$(call inherit-product-if-exists, vendor/motorola/shamu/device-vendor.mk)
