0d83452e5ca4b743da96f1db52dc894fe10a52ab
spaceman860 <spaceman8583@gmail.com>
2015-11-05 00:44:34 +0000

    Update fingerprint to MRA58N
    Change-Id: I37fe66088fb1f13bd4ef99394b2e421d1f070fd6

--- du.mk
+++ du.mk
@@ -33,8 +33,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
-BUILD_FINGERPRINT=google/shamu/shamu:6.0/MRA58K/2256973:user/release-keys \
-PRIVATE_BUILD_DESC="shamu-user 6.0 MRA58K 2256973 release-keys"
+BUILD_FINGERPRINT="google/shamu/shamu:6.0/MRA58N/2289998:user/release-keys" \
+PRIVATE_BUILD_DESC="shamu-user 6.0 MRA58N 2289998 release-keys"
 
 $(call inherit-product, device/moto/shamu/device.mk)
 $(call inherit-product-if-exists, vendor/motorola/shamu/device-vendor.mk)
