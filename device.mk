9eb713696a09eebe0f8bd3cd889494cfd5121122
beanstown106 <nbenis106@gmail.com>
2015-12-01 12:07:54 -0500

    shamu: add facelock and drm props

--- device.mk
+++ device.mk
@@ -169,6 +169,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.audio.fluence.speaker=false \
     ro.audio.monitorRotation=true
 
+# drmservice props
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
+# facelock props
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.facelock.black_timeout=400 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false
+
 # Audio effects
 PRODUCT_PACKAGES += \
     libqcomvisualizer \
