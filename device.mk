0d40c6753e9e57a407bfd12be6a9a2a0c8acd7cd
moepda <moepda@gmail.com>
2016-05-24 17:00:27 +0100

    Shamu : Move from xxhdpi to xxxhdpi setup
    Only in conjunction with:
    
    http://gerrit.dirtyunicorns.com/#/c/29527/
    http://gerrit.dirtyunicorns.com/#/c/29528/
    
    Change-Id: Ia665249ccd922ac85ac5a84dc2c47df5c9ed7730
    Signed-off-by: moepda <moepda@gmail.com>

--- device.mk
+++ device.mk
@@ -340,8 +340,8 @@ endif
 AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
 
 # setup dalvik vm configs.
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-hwui-memory.mk)
 
 $(call inherit-product-if-exists, hardware/qcom/msm8x84/msm8x84.mk)
 $(call inherit-product-if-exists, vendor/qcom/gpu/msm8x84/msm8x84-gpu-vendor.mk)
