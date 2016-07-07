84edf9c1f794a163da4347b92163c69fd4e5859e
spaceman860 <spaceman8583@gmail.com>
2016-07-07 09:57:02 -0400

    Shamu: update fingerprint to MOB30O
    Change-Id: I2d4c0a0f3a17b3c9e484673da24f90b1a0a285dd

--- du.mk
+++ du.mk
@@ -33,8 +33,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
-BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30M/2862625:user/release-keys \
-PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30M 2862625 release-keys"
+BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30O/2920157:user/release-keys \
+PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30O 2920157 release-keys"
 
 $(call inherit-product, device/moto/shamu/device.mk)
 $(call inherit-product-if-exists, vendor/motorola/shamu/device-vendor.mk)
