8aeb7154476bc559726cc5c01869de0818990081
beanstown106 <nbenis106@gmail.com>
2016-10-06 13:48:16 -0400

    add missing mobile data provisioning prop

--- device.mk
+++ device.mk
@@ -238,7 +238,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.data_no_toggle=1 \
     persist.radio.sib16_support=1 \
     persist.data.qmi.adb_logmask=0 \
-    persist.radio.alt_mbn_name=tmo_alt.mbn
+    persist.radio.alt_mbn_name=tmo_alt.mbn \
+    ro.com.android.prov_mobiledata=false
 
 # never dexopt the MotoSignature
 $(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
