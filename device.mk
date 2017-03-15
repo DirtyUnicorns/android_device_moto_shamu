a2667eb68dd976702df041d1ace8c6a03ebe6610
Fabien Sanglard <sanglardf@google.com>
2017-03-15 15:07:06 -0700

    Enable HWComposer service
    Test: AUPT
    Change-Id: Ia679386a2ee3af9d618e7d72f5c23e5fab84c4b7

--- device.mk
+++ device.mk
@@ -148,6 +148,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
     hwcomposer.msm8084 \
+    android.hardware.graphics.composer@2.1-impl \
     memtrack.msm8084 \
     libqdutils \
     libqdMetaData
