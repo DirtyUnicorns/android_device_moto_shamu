47cd2d9fa6d0961b3da82dad0c183feab9427229
Jake Weinstein <xboxlover360@gmail.com>
2017-05-08 20:42:34 -0400

    Shamu: increase heapminfree to 2m
    * This change reduces GC significantly during app launch, thereby
      improving the first launch latency.
    
    Change-Id: I9da567e8dbb2e8ea6969fd4fb0437f5462d6c1be
    Signed-off-by: Lennart Wieboldt <lennart.1997@gmx.de>

--- device.mk
+++ device.mk
@@ -345,7 +345,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
    ro.hwui.text_large_cache_height=2048
 
 PRODUCT_PROPERTY_OVERRIDES += \
-   dalvik.vm.heapgrowthlimit=256m
+   dalvik.vm.heapgrowthlimit=256m \
+   dalvik.vm.heapminfree=2m 
 
 # In userdebug, add minidebug info the the boot image and the system server to support
 # diagnosing native crashes.
