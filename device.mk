61fea509b53314c20ce644a7ef6fe988583f5a6e
moepda <moepda@gmail.com>
2017-09-04 10:45:25 +0100

    shamu : add missing + symbol
    Change-Id: I9d212fec4e0b3c15522537a255f71118c46087dd
    Signed-off-by: moepda <moepda@gmail.com>

--- device.mk
+++ device.mk
@@ -121,7 +121,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 DEVICE_PACKAGE_OVERLAYS := \
     device/moto/shamu/overlay
 
-PRODUCT_PACKAGES := \
+PRODUCT_PACKAGES += \
     libwpa_client \
     hostapd \
     wificond \
