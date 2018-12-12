a1c8755ca4002ee1b81e73aacb853f075c5430f5
Elektroschmock <elektroschmock78@googlemail.com>
2018-12-12 17:26:18 +0100

    shamu: Remove dead build target
    * power.shamu is dead since we moved to the new binderized power HAL.
    
    Change-Id: I1614dae5a55c5318b6ed2439350fcc2e18dd1105

--- device.mk
+++ device.mk
@@ -164,10 +164,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nci-20795a10.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci-20795a10.conf
 
-# Power
-PRODUCT_PACKAGES += \
-    power.shamu
-
 # Ramdisk
  PRODUCT_PACKAGES += \
     fstab.shamu \
