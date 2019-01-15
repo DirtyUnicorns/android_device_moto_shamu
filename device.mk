dded353a33eaaa0fb8bbaea0588949c4f940f61f
nobe1976 <charlesmc76@gmail.com>
2019-01-14 19:02:29 -0500

    shamu: PROPERTY_OVERRIDES cleanup moves and adds
    * cheers elektroschmock for the extra info-moepda
    
    Change-Id: Ic64f41f0bde9bd350cc1a082b969ea5b22a0eb77

--- device.mk
+++ device.mk
@@ -227,24 +227,6 @@ $(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
 # Enable for volte call
 AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
 
-PRODUCT_PROPERTY_OVERRIDES += \
-   ro.hwui.texture_cache_size=72 \
-   ro.hwui.layer_cache_size=48 \
-   ro.hwui.r_buffer_cache_size=8 \
-   ro.hwui.path_cache_size=32 \
-   ro.hwui.gradient_cache_size=1 \
-   ro.hwui.drop_shadow_cache_size=6 \
-   ro.hwui.texture_cache_flushrate=0.4 \
-   ro.hwui.text_small_cache_width=1024 \
-   ro.hwui.text_small_cache_height=1024 \
-   ro.hwui.text_large_cache_width=2048 \
-   ro.hwui.text_large_cache_height=2048
-
-
-PRODUCT_PROPERTY_OVERRIDES += \
-   dalvik.vm.heapgrowthlimit=256m \
-   dalvik.vm.heapminfree=2m
-
 # setup dalvik vm configs.
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
@@ -267,15 +249,3 @@ PRODUCT_OEM_PROPERTIES := \
 
 # Treble packages
 $(call inherit-product, $(LOCAL_PATH)/treble.mk)
-
-# Properties going into default.prop
-
-# OEM Unlock reporting
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.oem_unlock_supported=1
-
-# Camera configuration
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    camera.disable_zsl_mode=0 \
-    persist.camera.HAL3.enabled=1 \
-    persist.camera.ois.disable=0
